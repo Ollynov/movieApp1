d9266d74dcce111519b9da43e458d2a13955116c
Aljosha Novakovic <ollynov@gmail.com>
2016-08-30 14:07:09 -0700

    Form complete

--- src/Submit.js
+++ src/Submit.js
@@ -12,7 +12,21 @@ class Submit extends Component {
     return (
       <div id='container'>
         <div>
-          <p>placeholder</p>
+          <form id="form">
+            <fieldset>
+              <legend>Submit your own movie</legend><br/><br/>
+              Movie Title:
+              <input type='text' name="TITLE" /><br/><br/><br/>
+              Rating (1-10):
+              <input type='text'/><br/><br/><br/>
+              Cover photo:
+              <input type='text'/><br/><br/><br/><br/>
+              <input type="submit" value="Submit"/>
+            </fieldset>
+         </form>
+        </div>
+        <div>
+          <p>We only accept the best, make sure it's a 7+ quality movie!!</p>
         </div>
       </div>
     );
@@ -20,3 +34,12 @@ class Submit extends Component {
 }
 
 export default Submit;
+
+// <fieldset>
+// <legend>Personal information:</legend>
+// First name:<br>
+// <input type="text" name="firstname" value="Mickey"><br>
+// Last name:<br>
+// <input type="text" name="lastname" value="Mouse"><br><br>
+// <input type="submit" value="Submit">
+// </fieldset>
